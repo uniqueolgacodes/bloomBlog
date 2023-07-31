519dddbea6355f89fd5c75ec6719bd89014637e7
UniqueOlga <flamezthrower49@gmail.com>
2023-07-31 13:32:59 +0100

    1:30PM MONDAY JULY 31ST 2023

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,20 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function navBar() {
-    var x = document.getElementById("blog_topnav");
-    if (x.className === "blog_topnav") {
-      x.className += "responsive";
-    } else {
-      x.className = "blog_topnav";
-    }
-  }
+// function navBar() {
+//   var x = document.getElementById("blog_topnav");
+//   if (x.className.indexOf("responsive") === -1) {
+//     x.className += " responsive";
+//   } else {
+//     x.className = x.className.replace(" responsive", "");
+//   }
+// }
+// }function navBar() {
+//     var x = document.getElementById("blog_topnav");
+//     if (x.className === "blog_topnav") {
+//       x.className += "responsive";
+//     } else {
+//       x.className = "blog_topnav";
+//     }
+//   }
+document.getElementById("navbar_icon").addEventListener("click", function(){
+  document.getElementById("dropdown_content").style.display = "block";
+});
